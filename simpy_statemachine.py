7833abc1d57535db461e84108324869635e573b2
Sietse Brouwer <s.brouwer@anchormen.nl>
2017-04-19 22:05:37 +0200

    simpy statemachine: we try to reenter the state coro when we should make a new one

--- simpy_statemachine.py
+++ simpy_statemachine.py
@@ -2,34 +2,51 @@ import simpy
 import types
 
 
+# * a dict of generators (not generator functions) to dip into
+
+
 class Car:
 
     def to_generator(self, f):
         coro = f()
         f.send(None)
+        return f
 
     def parking(self):
+        print('parking... at', self.env.now)
         parking_duration = 5
         yield self.env.timeout(parking_duration)
         return 'driving'
 
     def driving(self):
+        print('driving... at', self.env.now)
         driving_duration = 2
         yield self.env.timeout(driving_duration)
         return 'parking'
 
     def __init__(self, env):
         self.env = env
-        states = {
-            'parking': self.parking,
-            'driving': self.driving,
+        self.states = {
+            'parking': self.parking(),
+            'driving': self.driving(),
         }
-        next_state_name = 'parking'
+
+    def process(self):
+        state_name = 'parking'
+        state = self.states[state_name]
         while True:
-            next_state = states[next_state_name]()
-            next_state_name = next_state.send(None)
+            try:
+                yield next(state)
+            except StopIteration as e:
+                state_name = e.value
+                state = self.states[state_name]
+            except KeyboardInterrupt:
+                return
+
 
 if __name__ == '__main__':
+    import pudb
+    # pudb.set_trace()
     env = simpy.Environment()
-    env.process(Car(env))
+    env.process(Car(env).process())
     env.run(until=15)
