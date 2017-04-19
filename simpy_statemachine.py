1ef3a1ab12e355fc2a1e1d42ef8b6dfab63a8214
Sietse Brouwer <s.brouwer@anchormen.nl>
2017-04-19 23:18:52 +0200

    it works!

--- simpy_statemachine.py
+++ simpy_statemachine.py
@@ -27,26 +27,28 @@ class Car:
     def __init__(self, env):
         self.env = env
         self.states = {
-            'parking': self.parking(),
-            'driving': self.driving(),
+            'parking': self.parking,
+            'driving': self.driving,
         }
 
     def process(self):
+        # This is a trampoline function
         state_name = 'parking'
-        state = self.states[state_name]
+        # create a generator for the initial state
+        state = self.states[state_name]()
         while True:
             try:
+                # run the generator
                 yield next(state)
             except StopIteration as e:
-                state_name = e.value
-                state = self.states[state_name]
+                # The state has ended;
+                # create a generator for the new state
+                state = self.states[e.value]()
             except KeyboardInterrupt:
                 return
 
 
 if __name__ == '__main__':
-    import pudb
-    # pudb.set_trace()
     env = simpy.Environment()
     env.process(Car(env).process())
     env.run(until=15)
