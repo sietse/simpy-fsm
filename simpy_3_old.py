231c035bb5631f61cefcb6e94fccab5e67d1a8f9
Sietse Brouwer <s.brouwer@anchormen.nl>
2020-03-29 21:58:14 +0200

    simpy_3: show both old and new

---
+++ simpy_3_old.py
@@ -0,0 +1,25 @@
+import simpy
+
+
+def car(env, name, bcs, driving_time, charge_duration):
+    # Simulate driving to the BCS
+    yield env.timeout(driving_time)
+
+    # Request one of its charging spots
+    print('%s arriving at %d' % (name, env.now))
+    with bcs.request() as req:
+        yield req
+
+        # Charge the battery
+        print('%s starting to charge at %s' % (name, env.now))
+        yield env.timeout(charge_duration)
+        print('%s leaving the bcs at %s' % (name, env.now))
+
+
+if __name__ == '__main__':
+    env = simpy.Environment()
+    bcs = simpy.Resource(env, capacity=2)
+    for i in range(4):
+        env.process(car(env, 'Car %d' % i, bcs, i*2, 5))
+
+    env.run()
