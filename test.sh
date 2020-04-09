6e662273c908fba4314caeae960c8b54ff87a281
Sietse Brouwer <s.brouwer@anchormen.nl>
2020-04-09 23:37:31 +0200

    test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Test all scripts
+
+python=/home/sietse/lib/miniconda3/envs/sim/bin/python
+
+$python nested_state_machine.py > /dev/null &&
+    $python simpy_2_charging.py > /dev/null &&
+    $python simpy_3b_resource_manually.py > /dev/null &&
+    $python simpy_3_resource.py > /dev/null &&
+    $python simpy_4_machine_shop.py > /dev/null &&
+    echo "Success" ||
+    echo "Error"
