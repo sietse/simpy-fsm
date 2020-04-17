f27b6bb1cb56f5e0ad0d113763655b20c2ead69e
Sietse Brouwer <s.brouwer@anchormen.nl>
2020-04-17 13:43:40 +0200

    clean up new example 4 to be more similar to the old code.

--- examples/4-preemptive-resource/new.py
+++ examples/4-preemptive-resource/new.py
@@ -40,19 +40,14 @@ SIM_TIME = WEEKS * 7 * 24 * 60  # Simulation time in minutes
 UNIMPORTANT_WORK_TIME = 7.0
 
 
-def spy(x, label=""):
-    # print(label, x)
-    return x
-
-
 def time_per_part():
     """Return actual processing time for a concrete part."""
-    return spy(abs(random.normalvariate(PT_MEAN, PT_SIGMA)), label="normal")
+    return abs(random.normalvariate(PT_MEAN, PT_SIGMA))
 
 
 def time_to_failure():
     """Return time until next failure for a machine."""
-    return spy(random.expovariate(BREAK_MEAN), label="expo")
+    return random.expovariate(BREAK_MEAN)
 
 
 class Machine(FSM):
@@ -130,6 +125,7 @@ class MachineFailure(FSM):
 
 
 class UnimportantWork(FSM):
+    """The repairman's other (unimportant) job."""
     def __init__(self, env, initial_state="awaiting_repairman", *, repairman):
         self.repairman = repairman
         self.works_made = 0
@@ -150,7 +146,6 @@ class UnimportantWork(FSM):
         self.state = "awaiting_repairman"
         self.repairman_request = self.repairman.request(priority=2)
         x = yield self.repairman_request
-        print(env.now, x)
         return self.working
 
     def working(self, data):
@@ -161,9 +156,7 @@ class UnimportantWork(FSM):
         try:
             # Try to work on the job until it is done ...
             x = yield env.timeout(self.work_left)
-            print(env.now, x)
             if env.now >= 28:
-                # import ipdb; ipdb.set_trace()  # FIXME
                 pass
             # ... (a) if the repairman is not called away, control is
             #     yielded back to us at the time our work is done, and we
@@ -194,6 +187,15 @@ class UnimportantWork(FSM):
         )
 
 
+def snapshot(env, repairman, unimportant_work, machines):
+    broken_machines = [m.id for m in machines if m.broken]
+    repairman_processes = [request.proc for request in repairman.users]
+    repairman_at = [m.id for m in machines if m.process in repairman_processes] + (
+        ["unimportant_work"] if unimportant_work.process in repairman_processes else []
+    )
+    return f"Repairman at {repairman_at}; broken {broken_machines}; unimportant_work state {unimportant_work.state}, made {unimportant_work.works_made}"
+
+
 # Setup and start the simulation
 print("Machine shop")
 random.seed(RANDOM_SEED)  # This helps reproducing the results
@@ -201,35 +203,21 @@ random.seed(RANDOM_SEED)  # This helps reproducing the results
 # Create an environment and start the setup process
 env = simpy.Environment()
 repairman = simpy.PreemptiveResource(env, capacity=1)
-unimportant_work = UnimportantWork(env, repairman=repairman)
 machines = [
     Machine(env, id=i, name=process_name(i, of=NUM_MACHINES), repairman=repairman)
     for i in range(NUM_MACHINES)
 ]
-
-
-def snapshot(env, repairman, unimportant_work, machines):
-    broken_machines = [m.id for m in machines if m.broken]
-    repairman_processes = [request.proc for request in repairman.users]
-    repairman_at = [m.id for m in machines if m.process in repairman_processes] + (
-        ["unimportant_work"] if unimportant_work.process in repairman_processes else []
-    )
-    return f"Repairman at {repairman_at}; broken {broken_machines}; UW state {unimportant_work.state}, made {unimportant_work.works_made}"
-
+unimportant_work = UnimportantWork(env, repairman=repairman)
 
 # Execute!
-prev_info = ""
-while env.now < 200:
-    info = snapshot(env, repairman, unimportant_work, machines)
-    if info[4:] != prev_info[4:]:
-        prev_info = info
-        print(f"{round(env.now)} {info}")
-    env.step()
+env.run(until=SIM_TIME)
 
 # Analyis/results
 print("Machine shop results after %s weeks" % WEEKS)
 for machine in machines:
     print("%s made %d parts." % (machine.name, machine.parts_made))
+
+print(snapshot(env, repairman, unimportant_work, machines))
 
 
 # [1] Floating-point errors
