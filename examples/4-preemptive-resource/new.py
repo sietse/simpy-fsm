2e8d5fec27846397575e471572701fa4fd6eff41
Sietse Brouwer <s.brouwer@anchormen.nl>
2020-04-17 23:07:52 +0200

    Move memo on floating point time bug to a separate markdown file

--- examples/4-preemptive-resource/new.py
+++ examples/4-preemptive-resource/new.py
@@ -172,10 +172,11 @@ class UnimportantWork(FSM):
         # We record where we were, and then try working again -- that will
         # start when we get our repairman back.
         except simpy.Interrupt:
-            # FIXME This is vulnerable to floating-point errors that may lead to
-            # negative work_left values. See [1], below, for the explanation.
-            # For now, `finish_work_and_prepare_next()` regularly resets
-            # self.work_left to 0
+            # FIXME Expressing work_done and work_left as floats vulnerable to
+            # floating-point errors: it may lead to negative work_left values.
+            # See floating_point_bug.md for a full explanation. For now,
+            # `finish_work_and_prepare_next()` regularly resets
+            # self.work_left to 0.
             #
             # FIXME There should be a lint that prevents any references
             # to an `env` other than `self.env`: it's easy to forget the
@@ -227,58 +228,3 @@ for machine in machines:
     print("%s made %d parts." % (machine.name, machine.parts_made))
 
 print(snapshot(env, repairman, unimportant_work, machines))
-
-
-# [1] Floating-point errors
-#
-# == Summary ==
-#
-# After the following sequence
-#
-#     start = self.env.now
-#     # (yield, and resume after self.work_left ticks)
-#     work_done = self.env.now - start
-#     self.work_left = self.work_left - work_done,
-#
-# the new self.work_left is *not* going to be zero, and it's going to be more
-# and more different from zero as the simulation progresses.
-#
-# This is because env.now is an increasing floating point number, and math with
-# larger floating point numbers gives larger floating point errors.
-#
-#
-# == Possible solutions ==
-#
-# - Don't use a floating-point counter. Discretize time and represent it as an
-#   integer.
-#
-# - Track how the (work_left - 0) error grows over simulation time; if the
-#   simulation ends before the error grows too large, we don't care.
-#
-# - Just set work_left to 0 manually after each job, postpone investigation
-#   until we start seeing weird behaviour.
-#
-#
-# == Why this is so ==
-#
-# If computer arithmatic were exact, life would be simple: if we start at
-# `t = start`, with `work_left` minutes of work left, we'll be done at
-# `t = (start + work_left)`. In math:
-#
-#                     start + work_left == end
-#                             work_left == end - start
-#             work_left - (end - start) == 0
-#
-# But we're working with floating point math, and floating point numbers can
-# only approximate most values. Let's write `x = fp(x) + err` to represent that
-# x is approximated by its floating-point representation, but an error will
-# remain. If we rewrite the equations above for floating point variables, we
-# get this:
-#
-#                 fp(start) + fp(work_left) == fp(end) + err
-#                             fp(work_left) == fp(end) - fp(start) + err
-#     fp(work_left) - (fp(end) - fp(start)) == 0 + err
-#
-# As time passes, `start` and `end` are going to become larger, and `fp(start)`
-# and `fp(end)` are going to become less precise. So as time passes, `err` is
-# going to get larger.
